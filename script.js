fb93d68d6c1dc7bb8fe49fc518ecdb79e42caa30
m-calabresi <mattia.calabresi@gmail.com>
2022-03-19 13:05:24 +0100

    ENH: toggle hamburger menu no longer scrolsl page to top,optimized JS scripts

--- script.js
+++ script.js
@@ -1,88 +1,47 @@
 (function () {
     window.onload = checkMobileBehavior;
     window.onresize = checkMobileBehavior;
-    
-    let showMenu = false;
-    
+
     /**
      * Enables the mobile/desktop behavior depending on screen size.
      */
     function checkMobileBehavior() {
         let isMobile = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--is-mobile"));
-        let mobileToggle = document.querySelector(".nav-mobile-toggle");
         let navItems = document.querySelectorAll(".nav-item");
-        
+
+        document.querySelector(".nav-mobile-toggle").onclick = mobileToggleClick;
+        window.onscroll = scrollFunction;
+
         if (isMobile) {
-            mobileToggle.onclick = mobileToggleClick;
-            window.onscroll = mobileScrollFunction;
-            
             document.querySelector(".nav-mobile-items").append(...navItems);
         } else {
-            mobileToggle.onclick = null;
-            window.onscroll = desktopScrollFunction;
-            
             let navItemsContainer = document.querySelector(".nav-items");
             if (navItemsContainer.childElementCount === 2) {
                 navItemsContainer.append(...navItems);
             }
         }
     }
-    
+
     /**
      * On mobile-toggle click, show menu (if on top of the page),
      * or nothing (if not at the top)
      */
     function mobileToggleClick() {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-        
-        if (isPageScrolled()) {
-            showMenu = true;
-        } else {
-            document.querySelector(".nav-mobile-items").classList.toggle("visible");
-            document.querySelector(".nav-mobile-toggle").classList.toggle("active");
-        }
+        document.querySelector(".nav-mobile-items").classList.toggle("visible");
+        document.querySelector(".nav-mobile-toggle").classList.toggle("active");
     }
-    
-    /**
-     * When the user scrolls down from the top of the document:
-     *  - resize the navbar's padding and the logo's font size
-     *  - collapse hamburger menu (if expanded) [COMMENTED]
-     *  - scroll to top and expand hamburger menu (if user clicked on it and he is not at the page top)
-     */
-    function mobileScrollFunction() {
-        let navLogoImg = document.querySelector(".nav-logo img");
-        let navItems = document.querySelector(".nav-mobile-items");
-        let navToggle = document.querySelector(".nav-mobile-toggle");
-        
-        if (isPageScrolled()) {
-            navLogoImg.classList.add("shrink");
-            
-            // [COMMENTED]
-            // if (!showMenu) {
-            //     navItems.classList.remove("visible");
-            //     navToggle.classList.remove("active");
-            // }
-        } else {
-            navLogoImg.classList.remove("shrink");
-            
-            if (showMenu) {
-                showMenu = false;
-                navItems.classList.add("visible");
-                navToggle.classList.add("active");
-            }
-        }
-    }
-    
+
     /**
      * When the user scrolls down from the top of the document:
      * - resize the navbar's padding and the logo's font size
      */
-    function desktopScrollFunction() {
+    function scrollFunction() {
         document.querySelector(".nav-logo img").classList.toggle("shrink", isPageScrolled());
     }
-    
+
     /**
-     * Whether or not the visible portion of the screen is far from the top part of the page.
+     * Whether or not the visible portion of the screen is far
+     * from the top part of the page.
      */
     function isPageScrolled() {
         return document.body.scrollTop > 0 || document.documentElement.scrollTop > 0;
